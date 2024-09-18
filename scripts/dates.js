3ee099a5ed2ef31d37dffc628452b02761c4a377
Joseph P H Howell <justpureh2o@outlook.com>
2024-09-18 19:05:00 +0800

    Update dates.js

--- scripts/dates.js
+++ scripts/dates.js
@@ -2,7 +2,9 @@ const today = new Date();
 
 let mourn = isTheDate(12, 13) || isTheDate(5, 12) || isTheDate(9, 18);
 if (mourn) {
-    document.styleSheets[1].insertRule("html{filter:grayscale(1);-webkit-filter: grayscale(100%);-moz-filter: grayscale(100%);-ms-filter: grayscale(100%);-o-filter: grayscale(100%);}");
+    setInterval(function () {
+        document.styleSheets[1].insertRule("html{filter:grayscale(1);-webkit-filter: grayscale(100%);-moz-filter: grayscale(100%);-ms-filter: grayscale(100%);-o-filter: grayscale(100%);}");
+    }, 1000);
 }
 
 function isTheDate(month, date) {
