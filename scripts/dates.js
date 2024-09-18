2a8611c27e4807ac82520c3d32242f666144e742
Joseph P H Howell <justpureh2o@outlook.com>
2024-09-18 20:16:35 +0800

    Update dates.js

--- scripts/dates.js
+++ scripts/dates.js
@@ -1,13 +1,9 @@
-(function () {
-    const today = new Date();
-    let mourn = isTheDate(12, 13) || isTheDate(5, 12) || isTheDate(9, 18);
-    if (mourn) {
-        setInterval(() => {
-            document.styleSheets[1].insertRule("html{filter:grayscale(1);-webkit-filter: grayscale(100%);-moz-filter: grayscale(100%);-ms-filter: grayscale(100%);-o-filter: grayscale(100%);}");
-        }, 1000);
-    }
+const today = new Date();
+let mourn = isTheDate(12, 13) || isTheDate(5, 12) || isTheDate(9, 18);
+if (mourn) {
+    document.styleSheets[1].insertRule("html{filter:grayscale(1);-webkit-filter: grayscale(100%);-moz-filter: grayscale(100%);-ms-filter: grayscale(100%);-o-filter: grayscale(100%);}");
+}
     
-    function isTheDate(month, date) {
-        return today.getDate() === date && today.getMonth() + 1 === month;
-    }
-})();
+function isTheDate(month, date) {
+    return today.getDate() === date && today.getMonth() + 1 === month;
+}
