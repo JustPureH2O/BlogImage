a016bd7c2b36f9d1b612bbc6c6f27496edad37c5
Joseph P H Howell <justpureh2o@outlook.com>
2025-01-19 19:47:21 +0800

    Create tempReplace.js

---
+++ scripts/tempReplace.js
@@ -0,0 +1,8 @@
+(
+  setInterval(() => {
+    let element = document.getElementById('blogRunTimeSpan')
+    if (element !== null) {
+      element.textContent = element.textContent.replace('This blog has running', '本博客已运行')
+    }
+  }, 50)
+)()
