b15f3a0dd5239d75882358d55c3305a8f5aff57c
Joseph P H Howell <justpureh2o@outlook.com>
2025-01-22 17:13:49 +0800

    Update justpureh2o_footer.js

--- scripts/justpureh2o_footer.js
+++ scripts/justpureh2o_footer.js
@@ -4,9 +4,9 @@
     let anchor = document.getElementById('cnblogs_post_body')
     if (bar.length === 1) {
         if (bar[0].nextSibling.textContent.includes('上')) {
-            anchor.insertAdjacentHTML("afterend", `<div class="footer-justpureh2o"><div class"button-navi atLeft"><a href="${bar[0].href}" id="special-link">上一篇：${bar[0].nextElementSibling.textContent}</a></div></div>`)
+            anchor.insertAdjacentHTML("afterend", `<div class="footer-justpureh2o"><div class="button-navi atLeft"><a href="${bar[0].href}" id="special-link">上一篇：${bar[0].nextElementSibling.textContent}</a></div></div>`)
         } else {
-            anchor.insertAdjacentHTML("afterend", `<div class="footer-justpureh2o"><div class"button-navi atRight"><a href="${bar[0].href}" id="special-link">下一篇：${bar[0].nextElementSibling.textContent}</a></div></div>`)
+            anchor.insertAdjacentHTML("afterend", `<div class="footer-justpureh2o"><div class="button-navi atRight"><a href="${bar[0].href}" id="special-link">下一篇：${bar[0].nextElementSibling.textContent}</a></div></div>`)
         }
     } else {
         anchor.insertAdjacentHTML("afterend", `<div class="footer-justpureh2o"><div class="button-navi atLeft"><a href="${bar[0].href}" id="special-link">上一篇：${bar[0].nextElementSibling.textContent}</a></div><div class="button-navi atRight"><a href="${bar[1].href}" id="special-link">下一篇：${bar[1].nextElementSibling.textContent}</a></div></div>`)
