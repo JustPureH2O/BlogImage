12054feeb51bfeb8b8b468ccba4ce3aa97db85cd
Joseph P H Howell <justpureh2o@outlook.com>
2025-04-12 08:32:50 +0800

    Update checker404.js

--- scripts/checker404.js
+++ scripts/checker404.js
@@ -8,10 +8,10 @@
     let flag = false;
     if (host.includes('en.justpureh2o.cn')) {
         fetch(`${domain.replace(english, mandarin)}`).then(res => flag = res.status === 200);
-        html = `<div class="note warning" id="checker-holder" style="display: none;"><p><strong>Link Matcher/文章检索</strong></p><p>The page you searched for currently under English site is not found, but article with same ID does exist in Mandarin site. <a href="${domain.replace(english, mandarin)}">Click here</a> to read it in Mandarin if you want.</p><p>您想要阅读的页面在当前站点（英文站）不存在！但我们在主站（中文站）找到了具有相同标识符的文章。您可以<a href="${domain.replace(english, mandarin)}">点击此链接</a>以跳转至中文站的对应页面继续阅读。</p></div>`;
+        html = `<p><strong>Link Matcher/文章检索</strong></p><p>The page you searched for currently under English site is not found, but article with same ID does exist in Mandarin site. <a href="${domain.replace(english, mandarin)}">Click here</a> to read it in Mandarin if you want.</p><p>您想要阅读的页面在当前站点（英文站）不存在！但我们在主站（中文站）找到了具有相同标识符的文章。您可以<a href="${domain.replace(english, mandarin)}">点击此链接</a>以跳转至中文站的对应页面继续阅读。</p>`;
     } else {
         fetch(`${domain.replace(mandarin, english)}`).then(res => flag = res.status === 200);
-        html = `<div class="note warning" id="checker-holder" style="display: none;"><p><strong>Link Matcher/文章检索</strong></p><p>The page you searched for currently under Mandarin site is not found, but article with same ID does exist in English site. <a href="${domain.replace(mandarin, english)}">Click here</a> to read it in English if you want.</p><p>您想要阅读的页面在当前站点（中文站）不存在！但我们在分站（英文站）找到了具有相同标识符的文章。您可以<a href="${domain.replace(mandarin, english)}">点击此链接</a>以跳转至英文站的对应页面继续阅读。</p></div>`;
+        html = `<p><strong>Link Matcher/文章检索</strong></p><p>The page you searched for currently under Mandarin site is not found, but article with same ID does exist in English site. <a href="${domain.replace(mandarin, english)}">Click here</a> to read it in English if you want.</p><p>您想要阅读的页面在当前站点（中文站）不存在！但我们在分站（英文站）找到了具有相同标识符的文章。您可以<a href="${domain.replace(mandarin, english)}">点击此链接</a>以跳转至英文站的对应页面继续阅读。</p>`;
     }
     if (flag) {
         let holder = document.findElementById('checker-holder');
