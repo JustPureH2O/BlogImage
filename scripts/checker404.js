3e263f4de8d2db7e96aadaf59f030a93ecd0532b
Joseph P H Howell <justpureh2o@outlook.com>
2025-04-12 09:11:07 +0800

    Update checker404.js

--- scripts/checker404.js
+++ scripts/checker404.js
@@ -6,7 +6,10 @@ let html = ``;
 
 let flag = false;
 if (host.includes('en.justpureh2o.cn')) {
-    fetch(`${domain.replace(english, mandarin)}`).then(res => flag = res.status === 200 || res.status === 301; console.log(res.status);).catch(err => flag = false);
+    fetch(`${domain.replace(english, mandarin)}`).then(res => {
+        flag = res.status === 200 || res.status === 301; 
+        console.log(res.status);
+    }).catch(err => flag = false);
     html = `<p><strong>Link Matcher/文章检索</strong></p><p>The page you searched for currently under English site is not found, but article with same ID does exist in Mandarin site. <a href="${domain.replace(english, mandarin)}">Click here</a> to read it in Mandarin if you want.</p><p>您想要阅读的页面在当前站点（英文站）不存在！但我们在主站（中文站）找到了具有相同标识符的文章。您可以<a href="${domain.replace(english, mandarin)}">点击此链接</a>以跳转至中文站的对应页面继续阅读。</p>`;
 } else {
     fetch(`${domain.replace(mandarin, english)}`).then(res => flag = res.status === 200 || res.status === 301).catch(err => flag = false);
